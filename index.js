7149e9cbd23711f158f320ffc5a5f7728a42500e
WhySoS3rious <sydhelly@hotmail.com>
2019-01-21 15:22:56 +0100

    cleanup

--- index.js
+++ index.js
@@ -1,80 +1,67 @@
-//const Layer2App = require('./layer2App')
-const BlockTracker = require('eth-block-tracker')
-const SafeEventEmitter = require('safe-event-emitter')
-const deepEqual = require('deep-equal')
-
 const pluginRegistrar = require('eth-plugin-registrar')
 
-class Layer2AppTracker extends SafeEventEmitter {
+class PluginWrapper {
 
-  constructor (opts = {}) {
-    super()
-    this.userAddress = opts.userAddress || '0x0'
-    this.provider = opts.provider
-    const pollingInterval = opts.pollingInterval || 4000
-    this.blockTracker = new BlockTracker({
-      provider: this.provider,
-      pollingInterval,
-    })
-    this.networkId = opts.networkId
+  constructor (){
+    // this.userAddress = opts.userAddress || '0x0'
+    // this.provider = opts.provider
+    // const pollingInterval = opts.pollingInterval || 4000
+    // this.blockTracker = new BlockTracker({
+    //   provider: this.provider,
+    //   pollingInterval,
+    // })
+    // this.networkId = opts.networkId
 
-    const layer2Apps = opts.layer2Apps || []
+    // const layer2Apps = opts.layer2Apps || []
 
-    console.log("Layer2AppTracker: ", layer2Apps)
+    // console.log("Layer2AppTracker: ", layer2Apps)
 
-    this.layer2Apps = layer2Apps.map((layer2AppOpts) => {
-      const app = this.createLayer2AppFrom(layer2AppOpts)
-      return app
-    })
+    // this.layer2Apps = layer2Apps.map((layer2AppOpts) => {
+    //   const app = this.createLayer2AppFrom(layer2AppOpts)
+    //   return app
+    // })
 
-    console.log("Layer2AppTracker after create: ", layer2Apps)
+    // console.log("Layer2AppTracker after create: ", layer2Apps)
     
-    this.running = true
-    this.blockTracker.on('latest', this.updateBalances.bind(this))
-    this.blockTracker.start()
-  }
-
-  serialize() {
-    return this.layer2Apps.map(layer2App => layer2App.serialize())
+    // this.running = true
+    // this.blockTracker.on('latest', this.updateBalances.bind(this))
+    // this.blockTracker.start()
   }
 
   async updateBalances() {
-    const oldBalances = this.serialize()
-    try {
-      await Promise.all(this.layer2Apps.map((layer2App) => {
-        return layer2App.updateBalance()
-      }))
+    // const oldBalances = this.serialize()
+    // try {
+    //   await Promise.all(this.layer2Apps.map((layer2App) => {
+    //     return layer2App.updateBalance()
+    //   }))
 
-      const newBalances = this.serialize()
-      if (!deepEqual(newBalances, oldBalances)) {
-        if (this.running) {
-          this.emit('update', newBalances)
-        }
-      }
-    } catch (reason) {
-      this.emit('error', reason)
-    }
+    //   const newBalances = this.serialize()
+    //   if (!deepEqual(newBalances, oldBalances)) {
+    //     if (this.running) {
+    //       this.emit('update', newBalances)
+    //     }
+    //   }
+    // } catch (reason) {
+    //   this.emit('error', reason)
+    // }
   }
 
   createLayer2AppFrom (opts) {
-    console.log(opts)
-    const owner = this.userAddress
-    const { address, name, balance, nodeUrl } = opts
-    const provider = this.provider
-    const blockTracker = this.blockTracker
-    const networkId = this.networkId
+    // console.log(opts)
+    // const owner = this.userAddress
+    // const { address, name, balance, nodeUrl } = opts
+    // const provider = this.provider
+    // const blockTracker = this.blockTracker
+    // const networkId = this.networkId
 //    return new Layer2App({ address, name, nodeUrl, balance, owner, provider, blockTracker, networkId })
   }
 
   add(opts) {
-    const layer2App = this.createLayer2AppFrom(opts)
-    this.layer2Apps.push(layer2App)
+    // const layer2App = this.createLayer2AppFrom(opts)
+    // this.layer2Apps.push(layer2App)
   }
 
-  stop(){
-    this.running = false
-    this.blockTracker.stop()
-  }
+  
 }
 
-module.exports = Layer2AppTracker
+module.exports = PluginWrapper
